1851ee537ebb122e77c5c6cd52d8dad71cb8622e
kmcclive <kenny.mcclive@me.com>
2020-07-01 17:19:00 -0600

    Fix has_many associations with embedded structs (#2)
    * Fix has_many association in embedded struct
    
    * Remove need for association_foreignkey with embedded has_many.
    
    * Fix more calls to getForeignField
    
    * Rename test models for consistency

--- model_struct_test.go
+++ model_struct_test.go
@@ -31,6 +31,22 @@ type ModelC struct {
 	OtherB   *ModelB `gorm:"foreignkey:OtherBID"`
 }
 
+type RequestModel struct {
+	Name     string
+	Children []ChildModel `gorm:"foreignkey:ParentID"`
+}
+
+type ChildModel struct {
+	ID       string
+	ParentID string
+	Name     string
+}
+
+type ResponseModel struct {
+	gorm.Model
+	RequestModel
+}
+
 // This test will try to cause a race condition on the model's foreignkey metadata
 func TestModelStructRaceSameModel(t *testing.T) {
 	// use a WaitGroup to execute as much in-sync as possible
@@ -90,4 +106,35 @@ func TestModelStructRaceDifferentModel(t *testing.T) {
 	}
 
 	done.Wait()
+}
+
+func TestModelStructEmbeddedHasMany(t *testing.T) {
+	fields := DB.NewScope(&ResponseModel{}).GetStructFields()
+
+	var childrenField *gorm.StructField
+
+	for i := 0; i < len(fields); i++ {
+		field := fields[i]
+
+		if field != nil && field.Name == "Children" {
+			childrenField = field
+		}
+	}
+
+	if childrenField == nil {
+		t.Error("childrenField should not be nil")
+		return
+	}
+
+	if childrenField.Relationship == nil {
+		t.Error("childrenField.Relation should not be nil")
+		return
+	}
+
+	expected := "has_many"
+	actual := childrenField.Relationship.Kind
+
+	if actual != expected {
+		t.Errorf("childrenField.Relationship.Kind should be %v, but was %v", expected, actual)
+	}
 }
